0436cfd57e3b23381697b3e71f1756e699b8d309
kolpakovee <eekolpakov@edu.hse.ru>
2024-10-10 13:01:02 +0300

    init

---
+++ query-dsl-demo-project/src/main/java/ru/kolpakovee/querydsldemoproject/QueryDslDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.kolpakovee.querydsldemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslDemoProjectApplication.class, args);
+	}
+
+}
